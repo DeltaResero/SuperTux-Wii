a5cc1e4216a0bd38ae80d3188755c2a3b7132b47
DeltaResero <deltaresero@zoho.com>
2017-02-28 10:38:16 -0400

    Add .orig to Autogen-clean script

--- autogen-clean.sh
+++ autogen-clean.sh
@@ -6,4 +6,5 @@ fi
 echo "Removing autogen generated files..."
 rm -f aclocal.m4 config.guess config.sub configure depcomp Makefile install-sh missing mkinstalldirs ltmain.sh stamp-h.in */Makefile.in */Makefile ltconfig stamp-h config.h.in
 rm -f -r autom4te.cache
+find . -name "*.orig" -type f -delete
 echo "Done."
