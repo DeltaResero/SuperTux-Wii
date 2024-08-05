45457fddc7b904a9a172a3c4c7f1676189d30f70
DeltaResero <3397617+DeltaResero@users.noreply.github.com>
2024-08-05 19:52:53 -0300

    Remove more Temporary Files

--- autogen-clean.sh
+++ autogen-clean.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 if [ -f Makefile ]; then
-	echo "Making make clean..."
-	make clean
+    echo "Making make clean..."
+    make clean
 fi
 echo "Removing autogen generated files..."
-rm -f aclocal.m4 config.guess config.sub configure depcomp Makefile install-sh missing mkinstalldirs ltmain.sh stamp-h.in */Makefile.in */Makefile ltconfig stamp-h config.h.in
+rm -f aclocal.m4 compile config.guess config.log config.status config.sub configure depcomp Makefile install-sh missing mkinstalldirs ltmain.sh stamp-h.in */Makefile.in */Makefile ltconfig stamp-h config.h.in
+rm -rf src/.deps
 rm -f -r autom4te.cache
 find . -name "*.orig" -type f -delete
 echo "Done."
