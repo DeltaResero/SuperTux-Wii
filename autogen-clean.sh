1a9f6e072c4ee09f6572f5c0386516baca14b061
DeltaResero <3397617+DeltaResero@users.noreply.github.com>
2024-08-07 02:16:38 -0300

    Boost Cleanup Script Efficiency

--- autogen-clean.sh
+++ autogen-clean.sh
@@ -1,11 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
+# Check if Makefile exists & run make clean if it does
 if [ -f Makefile ]; then
-    echo "Making make clean..."
+    echo "Running make clean..."
     make clean
 fi
+
+# Remove autogen generated and other temporary files
 echo "Removing autogen generated files..."
 rm -f aclocal.m4 compile config.guess config.log config.status config.sub configure depcomp Makefile install-sh missing mkinstalldirs ltmain.sh stamp-h.in */Makefile.in */Makefile ltconfig stamp-h config.h.in
 rm -rf src/.deps
-rm -f -r autom4te.cache
-find . -name "*.orig" -type f -delete
+rm -rf autom4te.cache
+
+# Find and delete all .orig files
+find . -name "*.orig" -type f -exec rm -f {} +
+
 echo "Done."
