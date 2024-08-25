59b73fe5e5ef770b29c780befe55a508d98a303e
DeltaResero <3397617+DeltaResero@users.noreply.github.com>
2024-08-25 11:50:25 -0300

    Modernize Sprite Handling & Enhance Documentation

--- src/sprite.cpp
+++ src/sprite.cpp
@@ -18,62 +18,82 @@
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #include "globals.h"
 #include "sprite.h"
 #include "setup.h"
 
+/**
+ * Constructs a Sprite object.
+ * Initializes the sprite based on the provided Lisp object, loading images and setting parameters.
+ * @param cur Pointer to the Lisp object representing the sprite data.
+ */
 Sprite::Sprite(lisp_object_t* cur)
 {
   init_defaults();
 
   LispReader reader(cur);
 
-  if(!reader.read_string("name",   &name))
-    st_abort("Sprite wihtout name", "");
+  if (!reader.read_string("name", &name))
+    st_abort("Sprite without name", "");
   reader.read_int("x-hotspot", &x_hotspot);
   reader.read_int("y-hotspot", &y_hotspot);
-  reader.read_float("fps",     &fps);
+  reader.read_float("fps", &fps);
 
   std::vector<std::string> images;
-  if(!reader.read_string_vector("images", &images))
+  if (!reader.read_string_vector("images", &images))
     st_abort("Sprite contains no images: ", name.c_str());
 
-  for(std::vector<std::string>::size_type i = 0; i < images.size(); ++i)
-    {
-      surfaces.push_back(
-          new Surface(datadir + "/images/" + images[i], USE_ALPHA));
-    }
+  for (const auto& image : images)
+  {
+    surfaces.push_back(
+        new Surface(datadir + "/images/" + image, USE_ALPHA));
+  }
 
-  frame_delay = 1000.0f/fps;
+  frame_delay = 1000.0f / fps;
 }
 
+/**
+ * Destroys the Sprite object.
+ * Frees the memory allocated for the surfaces associated with the sprite.
+ */
 Sprite::~Sprite()
 {
-  for(std::vector<Surface*>::iterator i = surfaces.begin(); i != surfaces.end();
-      ++i)
-    delete *i;
+  for (auto* surface : surfaces)
+    delete surface;
 }
 
-void
-Sprite::init_defaults()
+/**
+ * Initializes the default values for the sprite.
+ * This function sets default values for hotspot coordinates, frames per second, and timing.
+ */
+void Sprite::init_defaults()
 {
   x_hotspot = 0;
   y_hotspot = 0;
   fps = 10;
   time = 0;
-  frame_delay = 1000.0f/fps;
+  frame_delay = 1000.0f / fps;
 }
 
-void
-Sprite::update(float /*delta*/)
+/**
+ * Updates the sprite state.
+ * This function would normally update the sprite based on the elapsed time, but is currently disabled.
+ * @param delta The time that has passed since the last update.
+ */
+void Sprite::update(float /*delta*/)
 {
-  //time += 10*delta;
-  //std::cout << "Delta: " << delta << std::endl;
+  // time += 10 * delta;
+  // std::cout << "Delta: " << delta << std::endl;
 }
 
-void
-Sprite::draw(float x, float y)
+/**
+ * Draws the sprite at the specified coordinates.
+ * The sprite is drawn based on the current frame, adjusted by the hotspot coordinates.
+ * @param x The x-coordinate to draw the sprite.
+ * @param y The y-coordinate to draw the sprite.
+ */
+void Sprite::draw(float x, float y)
 {
   time = SDL_GetTicks();
   unsigned int frame = get_current_frame();
@@ -82,8 +102,17 @@ Sprite::draw(float x, float y)
     surfaces[frame]->draw(x - x_hotspot, y - y_hotspot);
 }
 
-void
-Sprite::draw_part(float sx, float sy, float x, float y, float w, float h)
+/**
+ * Draws a portion of the sprite at the specified coordinates.
+ * This function draws a specific part of the sprite, useful for animations or spritesheets.
+ * @param sx The source x-coordinate within the sprite.
+ * @param sy The source y-coordinate within the sprite.
+ * @param x The destination x-coordinate to draw the sprite.
+ * @param y The destination y-coordinate to draw the sprite.
+ * @param w The width of the portion to draw.
+ * @param h The height of the portion to draw.
+ */
+void Sprite::draw_part(float sx, float sy, float x, float y, float w, float h)
 {
   time = SDL_GetTicks();
   unsigned int frame = get_current_frame();
@@ -92,27 +121,40 @@ Sprite::draw_part(float sx, float sy, float x, float y, float w, float h)
     surfaces[frame]->draw_part(sx, sy, x - x_hotspot, y - y_hotspot, w, h);
 }
 
-void
-Sprite::reset()
+/**
+ * Resets the sprite's animation timer.
+ * This function resets the internal time to 0, restarting the animation from the first frame.
+ */
+void Sprite::reset()
 {
   time = 0;
 }
 
-int
-Sprite::get_current_frame() const
+/**
+ * Gets the current frame of the sprite based on the elapsed time.
+ * Calculates the current frame to display based on the time and frame delay.
+ * @return int The index of the current frame to display.
+ */
+int Sprite::get_current_frame() const
 {
-  unsigned int frame = static_cast<int>(fmodf(time, surfaces.size()*frame_delay)/frame_delay);
+  unsigned int frame = static_cast<int>(std::fmod(time, surfaces.size() * frame_delay) / frame_delay);
   return frame % surfaces.size();
 }
 
-int
-Sprite::get_width() const
+/**
+ * Gets the width of the current sprite frame.
+ * @return int The width of the current frame in pixels.
+ */
+int Sprite::get_width() const
 {
   return surfaces[get_current_frame()]->w;
 }
 
-int
-Sprite::get_height() const
+/**
+ * Gets the height of the current sprite frame.
+ * @return int The height of the current frame in pixels.
+ */
+int Sprite::get_height() const
 {
   return surfaces[get_current_frame()]->h;
 }
--- src/sprite.h
+++ src/sprite.h
@@ -1,5 +1,5 @@
 //  $Id: sprite.h 737 2004-04-26 12:21:23Z grumbel $
-// 
+//
 //  SuperTux
 //  Copyright (C) 2004 Ingo Ruhnke <grumbel@gmx.de>
 //
@@ -12,7 +12,7 @@
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -25,47 +25,89 @@
 #include "lispreader.h"
 #include "texture.h"
 
+/**
+ * Represents a 2D sprite with animation support.
+ * Manages loading, updating, and rendering of sprite frames.
+ */
 class Sprite
 {
  private:
-  std::string name;
+  std::string name;              // Sprite name
+  int x_hotspot;                 // X coordinate of the hotspot
+  int y_hotspot;                 // Y coordinate of the hotspot
+  float fps;                     // Frames per second for animation
+  float frame_delay;             // Frame duration in seconds
+  float time;                    // Time elapsed for current animation
+  std::vector<Surface*> surfaces; // Surfaces representing sprite frames
 
-  int x_hotspot;
-  int y_hotspot;
+  void init_defaults();          // Initialize default values for the sprite
 
-  /** Frames per second */
-  float fps;
-
-  /** Number of seconds that a frame is displayed until it is switched
-      to the next frame */
-  float frame_delay;
-
-  float time;
-
-  std::vector<Surface*> surfaces;
-
-  void init_defaults();
  public:
-  /** cur has to be a pointer to data in the form of ((x-hotspot 5)
-      (y-hotspot 10) ...) */
+  /**
+   * Constructs a Sprite from Lisp data.
+   * @param cur Pointer to Lisp data structure.
+   */
   Sprite(lisp_object_t* cur);
+
+  /**
+   * Destroys the Sprite object and frees allocated resources.
+   */
   ~Sprite();
-  
+
+  /**
+   * Resets the sprite's animation timer.
+   */
   void reset();
 
-  /** Update the sprite and process to the next frame */
+  /**
+   * Updates the sprite animation based on the time delta.
+   * @param delta Time passed since the last update.
+   */
   void update(float delta);
+
+  /**
+   * Draws the sprite at the specified coordinates.
+   * @param x X coordinate to draw the sprite.
+   * @param y Y coordinate to draw the sprite.
+   */
   void draw(float x, float y);
+
+  /**
+   * Draws a portion of the sprite at the specified coordinates.
+   * @param sx Source X coordinate within the sprite.
+   * @param sy Source Y coordinate within the sprite.
+   * @param x Destination X coordinate to draw the sprite.
+   * @param y Destination Y coordinate to draw the sprite.
+   * @param w Width of the portion to draw.
+   * @param h Height of the portion to draw.
+   */
   void draw_part(float sx, float sy, float x, float y, float w, float h);
+
+  /**
+   * Gets the current frame index based on the elapsed time.
+   * @return int The index of the current frame.
+   */
   int get_current_frame() const;
 
-  std::string get_name() const { return name; } 
+  /**
+   * Gets the name of the sprite.
+   * @return std::string The name of the sprite.
+   */
+  std::string get_name() const { return name; }
+
+  /**
+   * Gets the width of the current sprite frame.
+   * @return int The width of the current frame in pixels.
+   */
   int get_width() const;
+
+  /**
+   * Gets the height of the current sprite frame.
+   * @return int The height of the current frame in pixels.
+   */
   int get_height() const;
 };
 
 #endif
 
-/* Local Variables: */
-/* mode:c++ */
-/* End: */
+// EOF
