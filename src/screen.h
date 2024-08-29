85fbec527d12e805110478fd21e7ef5031f7ec9e
DeltaResero <3397617+DeltaResero@users.noreply.github.com>
2024-08-29 19:39:41 -0300

    Add docstring comments and cleanup screen.h & setup.h

--- src/screen.h
+++ src/screen.h
@@ -26,17 +26,21 @@
 #endif
 #include "texture.h"
 
-#define NO_UPDATE false
-#define UPDATE true
-#define USE_ALPHA 0
-#define IGNORE_ALPHA 1
+// Constants
+#define NO_UPDATE     false
+#define UPDATE        true
+#define USE_ALPHA     0
+#define IGNORE_ALPHA  1
 
+/**
+ * Struct representing a color with red, green, and blue components.
+ */
 struct Color
 {
-  Color() 
+  Color()
     : red(0), green(0), blue(0)
   {}
-  
+
   Color(int red_, int green_, int blue_)
     : red(red_), green(green_), blue(blue_)
   {}
@@ -44,15 +48,86 @@ struct Color
   int red, green, blue;
 };
 
+/**
+ * Draws a line on the screen.
+ *
+ * @param x1 The starting x-coordinate of the line.
+ * @param y1 The starting y-coordinate of the line.
+ * @param x2 The ending x-coordinate of the line.
+ * @param y2 The ending y-coordinate of the line.
+ * @param r  The red component of the line color.
+ * @param g  The green component of the line color.
+ * @param b  The blue component of the line color.
+ * @param a  The alpha component of the line color (transparency).
+ */
 void drawline(int x1, int y1, int x2, int y2, int r, int g, int b, int a);
+
+/**
+ * Clears the entire screen with a solid color.
+ *
+ * @param r The red component of the color.
+ * @param g The green component of the color.
+ * @param b The blue component of the color.
+ */
 void clearscreen(int r, int g, int b);
+
+/**
+ * Draws a vertical gradient across the screen from top to bottom.
+ *
+ * @param top_clr The color at the top of the screen.
+ * @param bot_clr The color at the bottom of the screen.
+ */
 void drawgradient(Color top_clr, Color bot_clr);
+
+/**
+ * Fills a rectangle with a solid color.
+ *
+ * @param x The x-coordinate of the top-left corner of the rectangle.
+ * @param y The y-coordinate of the top-left corner of the rectangle.
+ * @param w The width of the rectangle.
+ * @param h The height of the rectangle.
+ * @param r The red component of the color.
+ * @param g The green component of the color.
+ * @param b The blue component of the color.
+ * @param a The alpha component of the color (transparency).
+ */
 void fillrect(float x, float y, float w, float h, int r, int g, int b, int a);
-//void black_fade(Surface* surface, int seconds, bool fade_out);
+
+/**
+ * Fades the screen in or out using a specific surface.
+ *
+ * @param surface The surface to fade in or out.
+ * @param seconds The duration of the fade effect in seconds.
+ * @param fade_out Whether to fade out (true) or fade in (false).
+ */
 void fade(const std::string& surface, int seconds, bool fade_out);
+
+/**
+ * Updates the screen by redrawing the content.
+ */
 void updatescreen(void);
+
+/**
+ * Flips the screen buffers, updating the display with the drawn content.
+ */
 void flipscreen(void);
+
+/**
+ * Updates a specific rectangular area of the screen.
+ *
+ * @param scr The SDL surface representing the screen.
+ * @param x   The x-coordinate of the top-left corner of the rectangle.
+ * @param y   The y-coordinate of the top-left corner of the rectangle.
+ * @param w   The width of the rectangle.
+ * @param h   The height of the rectangle.
+ */
 void update_rect(SDL_Surface *scr, Sint32 x, Sint32 y, Sint32 w, Sint32 h);
+
+/**
+ * Fades the screen out to black.
+ */
 void fadeout();
 
-#endif /*SUPERTUX_SCREEN_H*/
+#endif /* SUPERTUX_SCREEN_H */
+
+// EOF
--- src/setup.h
+++ src/setup.h
@@ -24,31 +24,139 @@
 #include "sound.h"
 #include "type.h"
 
+/**
+ * Checks if the given file is accessible.
+ * @param filename The path to the file to check.
+ * @return int Returns 1 if accessible, 0 otherwise.
+ */
 int faccessible(const char *filename);
+
+/**
+ * Creates a directory relative to the current working directory.
+ * @param relative_dir The relative path of the directory to create.
+ * @return int Returns 0 on success, -1 on failure.
+ */
 int fcreatedir(const char* relative_dir);
+
+/**
+ * Checks if the given file is writable.
+ * @param filename The path to the file to check.
+ * @return int Returns 1 if writable, 0 otherwise.
+ */
 int fwriteable(const char *filename);
+
+/**
+ * Opens a data file with the specified mode.
+ * @param filename The path to the file to open.
+ * @param mode The mode to open the file in.
+ * @return FILE* A file pointer on success, or NULL on failure.
+ */
 FILE * opendata(const char * filename, const char * mode);
+
+/**
+ * Retrieves a list of subdirectories within the specified path that contain an expected file.
+ * @param rel_path The relative path to search within.
+ * @param expected_file The name of the expected file in the subdirectories.
+ * @return string_list_type A list of subdirectory names.
+ */
 string_list_type dsubdirs(const char *rel_path, const char* expected_file);
+
+/**
+ * Retrieves a list of files matching a pattern in the specified path.
+ * @param rel_path The relative path to search within.
+ * @param glob The pattern to match files against.
+ * @param exception_str A string describing any exceptions to log.
+ * @return string_list_type A list of file names.
+ */
 string_list_type dfiles(const char *rel_path, const char* glob, const char* exception_str);
+
+/**
+ * Frees an array of strings.
+ * @param strings The array of strings to free.
+ * @param num The number of strings in the array.
+ */
 void free_strings(char **strings, int num);
+
+/**
+ * Sets up the directory structure needed for the game.
+ */
 void st_directory_setup(void);
+
+/**
+ * General setup function for the game, including initializing subsystems.
+ */
 void st_general_setup(void);
+
+/**
+ * Frees resources allocated during general setup.
+ */
 void st_general_free();
+
+/**
+ * Sets up the SDL video subsystem.
+ */
 void st_video_setup_sdl(void);
+
+/**
+ * Sets up the OpenGL video subsystem.
+ */
 void st_video_setup_gl(void);
+
+/**
+ * Determines and sets up the appropriate video subsystem (SDL or OpenGL).
+ */
 void st_video_setup(void);
+
+/**
+ * Sets up the audio subsystem.
+ */
 void st_audio_setup(void);
+
+/**
+ * Sets up the joystick subsystem.
+ */
 void st_joystick_setup(void);
+
+/**
+ * Shuts down the game, freeing all resources.
+ */
 void st_shutdown(void);
+
+/**
+ * Displays the main menu of the game.
+ */
 void st_menu(void);
+
+/**
+ * Aborts the game with a given reason and detailed message.
+ * @param reason A short reason for the abort.
+ * @param details Additional details about the abort.
+ */
 void st_abort(const std::string& reason, const std::string& details);
+
+/**
+ * Processes the options menu, handling user input and updating settings.
+ */
 void process_options_menu(void);
 
-/** Return true if the gameloop() was entered, false otherwise */
+/**
+ * Returns true if the game loop was entered, false otherwise.
+ * @return bool True if the game loop was entered, false otherwise.
+ */
 bool process_load_game_menu();
 
+/**
+ * Updates the load/save game menu with the latest available options.
+ * @param pmenu Pointer to the menu to update.
+ */
 void update_load_save_game_menu(Menu* pmenu);
+
+/**
+ * Parses command-line arguments passed to the game.
+ * @param argc The number of arguments.
+ * @param argv The array of argument strings.
+ */
 void parseargs(int argc, char * argv[]);
 
-#endif /*SUPERTUX_SETUP_H*/
-
+#endif /* SUPERTUX_SETUP_H */
+// EOF
