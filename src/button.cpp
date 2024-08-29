8ea75cdc0cae80264d59785ae7bcfe06aba94230
DeltaResero <3397617+DeltaResero@users.noreply.github.com>
2024-08-29 19:33:37 -0300

    Cleanup, Document, Optimize, & Refactor Button Classes with Modern C++ practices

--- src/button.cpp
+++ src/button.cpp
@@ -18,8 +18,9 @@
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 //  02111-1307, USA.
 
-#include <string.h>
-#include <stdlib.h>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
 #include "setup.h"
 #include "screen.h"
 #include "globals.h"
@@ -27,14 +28,22 @@
 
 Timer Button::popup_timer;
 
+/**
+ * Constructor for the Button class.
+ * @param icon_file The file path of the icon.
+ * @param ninfo Information string to display.
+ * @param nshortcut Keyboard shortcut for the button.
+ * @param x The x-coordinate of the button.
+ * @param y The y-coordinate of the button.
+ * @param mw The width to resize the icon (default -1).
+ * @param mh The height to resize the icon (default -1).
+ */
 Button::Button(std::string icon_file, std::string ninfo, SDLKey nshortcut, int x, int y, int mw, int mh)
 {
   popup_timer.init(false);
-
-  add_icon(icon_file,mw,mh);
+  add_icon(icon_file, mw, mh);
 
   info = ninfo;
-
   shortcut = nshortcut;
 
   rect.x = x;
@@ -44,123 +53,152 @@ Button::Button(std::string icon_file, std::string ninfo, SDLKey nshortcut, int x
   tag = -1;
   state = BUTTON_NONE;
   show_info = false;
-  game_object = NULL;
+  game_object = nullptr;
 }
 
+/**
+ * Adds an icon to the button.
+ * @param icon_file The file path of the icon.
+ * @param mw The width to resize the icon.
+ * @param mh The height to resize the icon.
+ */
 void Button::add_icon(std::string icon_file, int mw, int mh)
 {
   char filename[1024];
 
-  if(!icon_file.empty())
+  if (!icon_file.empty())
   {
     snprintf(filename, 1024, "%s/%s", datadir.c_str(), icon_file.c_str());
-    if(!faccessible(filename))
+    if (!faccessible(filename))
+    {
       snprintf(filename, 1024, "%s/images/icons/default-icon.png", datadir.c_str());
+    }
   }
   else
   {
     snprintf(filename, 1024, "%s/images/icons/default-icon.png", datadir.c_str());
   }
 
-  if(mw != -1 || mh != -1)
-  {
-    icon.push_back(new Surface(filename,USE_ALPHA));
-    icon.back()->resize(mw,mh);
-  }
-  else
-    icon.push_back(new Surface(filename,USE_ALPHA));
+  icon.push_back(new Surface(filename, USE_ALPHA));
 
+  if (mw != -1 || mh != -1)
+  {
+    icon.back()->resize(mw, mh);
+  }
 }
 
+/**
+ * Draws the button on the screen.
+ */
 void Button::draw()
 {
-  if(state == BUTTON_HOVER)
-    if(!popup_timer.check())
-      show_info = true;
-
-  fillrect(rect.x,rect.y,rect.w,rect.h,75,75,75,200);
-  fillrect(rect.x+1,rect.y+1,rect.w-2,rect.h-2,175,175,175,200);
-
-  for(std::vector<Surface*>::iterator it = icon.begin(); it != icon.end(); ++it)
-    (*it)->draw(rect.x,rect.y);
-
-  if(game_object != NULL)
+  if (state == BUTTON_HOVER)
   {
-    game_object->draw_on_screen(rect.x,rect.y);
+    if (!popup_timer.check())
+    {
+      show_info = true;
+    }
   }
 
-  if(show_info)
+  fillrect(rect.x, rect.y, rect.w, rect.h, 75, 75, 75, 200);
+  fillrect(rect.x + 1, rect.y + 1, rect.w - 2, rect.h - 2, 175, 175, 175, 200);
+
+  for (auto& surface : icon)
+  {
+    surface->draw(rect.x, rect.y);
+  }
+
+  if (game_object != nullptr)
+  {
+    game_object->draw_on_screen(rect.x, rect.y);
+  }
+
+  if (show_info)
   {
     char str[80];
     int i = -32;
 
-    if(0 > rect.x - (int)strlen(info.c_str()) * white_small_text->w)
-      i = rect.w + strlen(info.c_str()) * white_small_text->w;
+    if (0 > rect.x - static_cast<int>(info.length()) * white_small_text->w)
+    {
+      i = rect.w + info.length() * white_small_text->w;
+    }
 
-    if(!info.empty())
-      white_small_text->draw(info.c_str(), i + rect.x - strlen(info.c_str()) * white_small_text->w, rect.y, 1);
-    sprintf(str,"(%s)", SDL_GetKeyName(shortcut));
-    white_small_text->draw(str, i + rect.x - strlen(str) * white_small_text->w, rect.y + white_small_text->h+2, 1);
+    if (!info.empty())
+    {
+      white_small_text->draw(info.c_str(), i + rect.x - static_cast<int>(info.length()) * white_small_text->w, rect.y, 1);
+    }
+
+    snprintf(str, sizeof(str), "(%s)", SDL_GetKeyName(shortcut));
+    white_small_text->draw(str, i + rect.x - static_cast<int>(strlen(str)) * white_small_text->w, rect.y + white_small_text->h + 2, 1);
   }
-  if(state == BUTTON_PRESSED || state == BUTTON_DEACTIVE)
-    fillrect(rect.x,rect.y,rect.w,rect.h,75,75,75,200);
-  else if(state == BUTTON_HOVER)
-    fillrect(rect.x,rect.y,rect.w,rect.h,150,150,150,128);
+
+  if (state == BUTTON_PRESSED || state == BUTTON_DEACTIVE)
+  {
+    fillrect(rect.x, rect.y, rect.w, rect.h, 75, 75, 75, 200);
+  }
+  else if (state == BUTTON_HOVER)
+  {
+    fillrect(rect.x, rect.y, rect.w, rect.h, 150, 150, 150, 128);
+  }
 }
 
+/**
+ * Destructor for the Button class.
+ */
 Button::~Button()
 {
-  for(std::vector<Surface*>::iterator it = icon.begin(); it != icon.end(); ++it)
-    delete (*it);
+  for (auto& surface : icon)
+  {
+    delete surface;
+  }
   icon.clear();
   delete game_object;
 }
 
+/**
+ * Handles SDL events for the button.
+ * @param event The SDL event to handle.
+ */
 void Button::event(SDL_Event &event)
 {
-  if(state == BUTTON_DEACTIVE)
+  if (state == BUTTON_DEACTIVE)
+  {
     return;
+  }
 
   SDLKey key = event.key.keysym.sym;
 
-  if(event.type == SDL_MOUSEBUTTONDOWN || event.type == SDL_MOUSEBUTTONUP)
+  if (event.type == SDL_MOUSEBUTTONDOWN || event.type == SDL_MOUSEBUTTONUP)
   {
-    if(event.button.x < rect.x || event.button.x >= rect.x + rect.w ||
-        event.button.y < rect.y || event.button.y >= rect.y + rect.h)
+    if (event.button.x < rect.x || event.button.x >= rect.x + rect.w || event.button.y < rect.y || event.button.y >= rect.y + rect.h)
+    {
       return;
+    }
 
-    if(event.button.button == SDL_BUTTON_RIGHT)
+    if (event.button.button == SDL_BUTTON_RIGHT)
     {
       show_info = true;
       return;
     }
-    else if(event.type == SDL_MOUSEBUTTONUP && event.button.button == 4) /* Mouse wheel up. */
+    else if (event.type == SDL_MOUSEBUTTONUP && event.button.button == 4) /* Mouse wheel up. */
     {
       state = BUTTON_WHEELUP;
       return;
     }
-    else if(event.type == SDL_MOUSEBUTTONUP && event.button.button == 5) /* Mouse wheel down. */
+    else if (event.type == SDL_MOUSEBUTTONUP && event.button.button == 5) /* Mouse wheel down. */
     {
       state = BUTTON_WHEELDOWN;
       return;
     }
 
-    if(event.button.button == SDL_BUTTON_LEFT)
+    if (event.button.button == SDL_BUTTON_LEFT)
     {
-      if(event.type == SDL_MOUSEBUTTONDOWN)
-      {
-        state = BUTTON_PRESSED;
-      }
-      else
-      {
-        state = BUTTON_CLICKED;
-      }
+      state = (event.type == SDL_MOUSEBUTTONDOWN) ? BUTTON_PRESSED : BUTTON_CLICKED;
     }
   }
-  else if(event.type == SDL_MOUSEMOTION)
+  else if (event.type == SDL_MOUSEMOTION)
   {
-    if(event.motion.x < rect.x || event.motion.x >= rect.x + rect.w ||
-        event.motion.y < rect.y || event.motion.y >= rect.y + rect.h)
+    if (event.motion.x < rect.x || event.motion.x >= rect.x + rect.w || event.motion.y < rect.y || event.motion.y >= rect.y + rect.h)
     {
       state = BUTTON_NONE;
     }
@@ -170,39 +208,63 @@ void Button::event(SDL_Event &event)
       popup_timer.start(1500);
     }
 
-    if(show_info)
+    if (show_info)
     {
       show_info = false;
     }
   }
-  else if(event.type == SDL_KEYDOWN)
+  else if (event.type == SDL_KEYDOWN)
   {
-    if(key == shortcut)
+    if (key == shortcut)
+    {
       state = BUTTON_PRESSED;
+    }
   }
-  else if(event.type == SDL_KEYUP)
+  else if (event.type == SDL_KEYUP)
   {
-    if(state == BUTTON_PRESSED && key == shortcut)
+    if (state == BUTTON_PRESSED && key == shortcut)
+    {
       state = BUTTON_CLICKED;
+    }
   }
 }
 
+/**
+ * Returns the current state of the button.
+ * @return The state of the button.
+ */
 int Button::get_state()
 {
   int rstate;
-  switch(state)
+  switch (state)
   {
-  case BUTTON_CLICKED:
-  case BUTTON_WHEELUP:
-  case BUTTON_WHEELDOWN:
-    rstate = state;
-    state = BUTTON_NONE;
-    return rstate;
-  default:
-    return state;
+    case BUTTON_CLICKED:
+    case BUTTON_WHEELUP:
+    case BUTTON_WHEELDOWN:
+      rstate = state;
+      state = BUTTON_NONE;
+      return rstate;
+    default:
+      return state;
   }
 }
 
+/**
+ * Sets the game object associated with this button.
+ * @param game_object_ Pointer to the game object.
+ */
+void Button::set_game_object(GameObject* game_object_)
+{
+  game_object = game_object_;
+}
+
+/**
+ * Constructor for the ButtonPanel class.
+ * @param x The x-coordinate of the panel.
+ * @param y The y-coordinate of the panel.
+ * @param w The width of the panel.
+ * @param h The height of the panel.
+ */
 ButtonPanel::ButtonPanel(int x, int y, int w, int h)
 {
   bw = 32;
@@ -215,55 +277,69 @@ ButtonPanel::ButtonPanel(int x, int y, int w, int h)
   hlast = false;
 }
 
+/**
+ * Handles SDL events for the panel and its buttons.
+ * @param event The SDL event to handle.
+ * @return The button that was interacted with, or nullptr if none.
+ */
 Button* ButtonPanel::event(SDL_Event& event)
 {
-  if(!hidden)
+  if (!hidden)
   {
-  Button* ret = NULL;
-    for(std::vector<Button*>::iterator it = item.begin(); it != item.end(); ++it)
+    Button* ret = nullptr;
+    for (auto& button : item)
     {
-      (*it)->event(event);
-      if((*it)->state != BUTTON_NONE)
+      button->event(event);
+      if (button->state != BUTTON_NONE)
       {
-        if(hlast && (*it)->state == BUTTON_CLICKED)
-          last_clicked = it;
-	ret = (*it);
+        if (hlast && button->state == BUTTON_CLICKED)
+        {
+          last_clicked = std::find(item.begin(), item.end(), button);
+        }
+        ret = button;
       }
     }
     return ret;
   }
-  else
-  {
-    return NULL;
-  }
+  return nullptr;
 }
 
+/**
+ * Destructor for the ButtonPanel class.
+ */
 ButtonPanel::~ButtonPanel()
 {
-  for(std::vector<Button*>::iterator it = item.begin(); it != item.end(); ++it)
+  for (auto& button : item)
   {
-    delete (*it);
+    delete button;
   }
   item.clear();
 }
 
+/**
+ * Draws the panel and its buttons on the screen.
+ */
 void ButtonPanel::draw()
 {
-
-  if(hidden == false)
+  if (!hidden)
   {
-    fillrect(rect.x,rect.y,rect.w,rect.h,100,100,100,200);
-    for(std::vector<Button*>::iterator it = item.begin(); it != item.end(); ++it)
+    fillrect(rect.x, rect.y, rect.w, rect.h, 100, 100, 100, 200);
+    for (auto& button : item)
     {
-      (*it)->draw();
-      if(hlast && it == last_clicked)
+      button->draw();
+      if (hlast && std::find(item.begin(), item.end(), button) == last_clicked)
       {
-        fillrect((*it)->get_pos().x,(*it)->get_pos().y,(*it)->get_pos().w,(*it)->get_pos().h,100,100,100,128);
+        fillrect(button->get_pos().x, button->get_pos().y, button->get_pos().w, button->get_pos().h, 100, 100, 100, 128);
       }
     }
   }
 }
 
+/**
+ * Adds a button to the panel.
+ * @param pbutton The button to add.
+ * @param tag A tag to identify the button.
+ */
 void ButtonPanel::additem(Button* pbutton, int tag)
 {
   int max_cols, row, col;
@@ -274,32 +350,23 @@ void ButtonPanel::additem(Button* pbutton, int tag)
 
   max_cols = rect.w / bw;
 
-  row = (item.size()-1) / max_cols;
-  col = (item.size()-1) % max_cols;
+  row = (item.size() - 1) / max_cols;
+  col = (item.size() - 1) % max_cols;
 
-  item[item.size()-1]->rect.x = rect.x + col * bw;
-  item[item.size()-1]->rect.y = rect.y + row * bh;
-  item[item.size()-1]->tag = tag;
-
+  item.back()->rect.x = rect.x + col * bw;
+  item.back()->rect.y = rect.y + row * bh;
+  item.back()->tag = tag;
 }
 
+/**
+ * Sets the size of the buttons in the panel.
+ * @param w The width of the buttons.
+ * @param h The height of the buttons.
+ */
 void ButtonPanel::set_button_size(int w, int h)
 {
   bw = w;
   bh = h;
 }
 
-Button* ButtonPanel::manipulate_button(int i)
-{
-  if(int(item.size())-1 < i)
-    return item[item.size()-1];
-  else
-    return item[i];
-}
-
-void ButtonPanel::highlight_last(bool b)
-{
-  hlast = b;
-}
-
-
+// EOF
--- src/button.h
+++ src/button.h
@@ -24,7 +24,8 @@
 #include <vector>
 #include "texture.h"
 
-enum ButtonState {
+enum ButtonState
+{
   BUTTON_NONE = -1,
   BUTTON_CLICKED,
   BUTTON_PRESSED,
@@ -36,57 +37,188 @@ enum ButtonState {
 
 class ButtonPanel;
 
+/**
+ * Button class represents a clickable UI element in the game.
+ */
 class Button
 {
   friend class ButtonPanel;
 
 public:
-  Button(std::string icon_file, std::string info, SDLKey shortcut, int x, int y, int mw = -1, int h = -1);
+  /**
+   * Constructor for the Button class.
+   * @param icon_file The file path of the icon.
+   * @param info The information string to display.
+   * @param shortcut The keyboard shortcut for the button.
+   * @param x The x-coordinate of the button.
+   * @param y The y-coordinate of the button.
+   * @param mw The width to resize the icon (default -1).
+   * @param mh The height to resize the icon (default -1).
+   */
+  Button(std::string icon_file, std::string info, SDLKey shortcut, int x, int y, int mw = -1, int mh = -1);
+
+  /**
+   * Destructor for the Button class.
+   */
   ~Button();
+
+  /**
+   * Handles SDL events for the button.
+   * @param event The SDL event to handle.
+   */
   void event(SDL_Event& event);
+
+  /**
+   * Draws the button on the screen.
+   */
   void draw();
+
+  /**
+   * Returns the current state of the button.
+   * @return The state of the button.
+   */
   int get_state();
-  void set_active(bool active) { active ? state = BUTTON_NONE : state = BUTTON_DEACTIVE; };
+
+  /**
+   * Sets the button to be active or inactive.
+   * @param active True to activate, false to deactivate.
+   */
+  void set_active(bool active) { active ? state = BUTTON_NONE : state = BUTTON_DEACTIVE; }
+
+  /**
+   * Adds an icon to the button.
+   * @param icon_file The file path of the icon.
+   * @param mw The width to resize the icon.
+   * @param mh The height to resize the icon.
+   */
   void add_icon(std::string icon_file, int mw, int mh);
-  SDL_Rect get_pos() { return rect; }
-  int get_tag(){return tag; }
-  void set_game_object(GameObject* game_object_) { game_object = game_object_; }
-  GameObject* get_game_object() { return game_object; };
+
+  /**
+   * Returns the position and size of the button.
+   * @return SDL_Rect structure containing the button's position and size.
+   */
+  inline SDL_Rect get_pos() const { return rect; }
+
+  /**
+   * Returns the tag associated with the button.
+   * @return The tag identifier.
+   */
+  inline int get_tag() const { return tag; }
+
+  /**
+   * Sets the game object associated with this button.
+   * @param game_object_ Pointer to the game object.
+   */
+  void set_game_object(GameObject* game_object_);
+
+  /**
+   * Returns the game object associated with this button.
+   * @return Pointer to the game object.
+   */
+  inline GameObject* get_game_object() const { return game_object; }
 
 private:
-  static Timer popup_timer;
-  GameObject* game_object;
-  std::vector<Surface*> icon;
-  std::string info;
-  SDLKey shortcut;
-  SDL_Rect rect;
-  bool show_info;
-  ButtonState state;
-  int tag;
+  static Timer popup_timer;            /**< Static timer for popup display */
+  GameObject* game_object;             /**< Pointer to the game object associated with this button */
+  std::vector<Surface*> icon;          /**< Icons associated with the button */
+  std::string info;                    /**< Information string to display */
+  SDLKey shortcut;                     /**< Keyboard shortcut for the button */
+  SDL_Rect rect;                       /**< Rectangle representing button's position and size */
+  bool show_info;                      /**< Flag indicating whether to show additional info */
+  ButtonState state;                   /**< Current state of the button */
+  int tag;                             /**< Tag identifier for the button */
 };
 
+/**
+ * ButtonPanel class represents a collection of buttons arranged within a panel.
+ */
 class ButtonPanel
 {
 public:
+  /**
+   * Constructor for the ButtonPanel class.
+   * @param x The x-coordinate of the panel.
+   * @param y The y-coordinate of the panel.
+   * @param w The width of the panel.
+   * @param h The height of the panel.
+   */
   ButtonPanel(int x, int y, int w, int h);
+
+  /**
+   * Destructor for the ButtonPanel class.
+   */
   ~ButtonPanel();
+
+  /**
+   * Draws the panel and its buttons on the screen.
+   */
   void draw();
-  Button* event(SDL_Event &event);
+
+  /**
+   * Handles SDL events for the panel and its buttons.
+   * @param event The SDL event to handle.
+   * @return Pointer to the button that was interacted with, or nullptr if none.
+   */
+  Button* event(SDL_Event& event);
+
+  /**
+   * Adds a button to the panel.
+   * @param pbutton Pointer to the button to add.
+   * @param tag A tag identifier to associate with the button.
+   */
   void additem(Button* pbutton, int tag);
-  Button* button_panel_event(SDL_Event& event);
+
+  /**
+   * Returns a pointer to the button at the specified index.
+   * @param i The index of the button.
+   * @return Pointer to the button at the specified index.
+   */
+  inline Button* manipulate_button(int i)
+  {
+    if (static_cast<int>(item.size()) - 1 < i)
+    {
+      return item.back();
+    }
+    return item[i];
+  }
+
+  /**
+   * Highlights the last clicked button.
+   * @param b Boolean flag to indicate whether to highlight the last clicked button.
+   */
+  inline void highlight_last(bool b)
+  {
+    hlast = b;
+  }
+
+  /**
+   * Sets the last clicked button to the specified index.
+   * @param last Index of the last clicked button.
+   */
+  inline void set_last_clicked(unsigned int last)
+  {
+    if (last < item.size())
+    {
+      last_clicked = item.begin() + last;
+    }
+  }
+
+  /**
+   * Sets the size of the buttons within the panel.
+   * @param w The width of the buttons.
+   * @param h The height of the buttons.
+   */
   void set_button_size(int w, int h);
-  Button* manipulate_button(int i);
-  void highlight_last(bool b);
-  void set_last_clicked(unsigned int last)
-  { if(hlast) { if(item.size() >= last) { last_clicked = item.begin() + last; } } };
 
 private:
-  int bw, bh;
-  bool hlast;
-  bool hidden;
-  SDL_Rect rect;
-  std::vector<Button*> item;
-  std::vector<Button*>::iterator last_clicked;
+  int bw, bh;                              /**< Width and height of the buttons */
+  bool hlast;                              /**< Flag indicating whether to highlight the last clicked button */
+  bool hidden;                             /**< Flag indicating whether the panel is hidden */
+  SDL_Rect rect;                           /**< Rectangle representing panel's position and size */
+  std::vector<Button*> item;               /**< Collection of buttons within the panel */
+  std::vector<Button*>::iterator last_clicked; /**< Iterator to the last clicked button */
 };
 
-#endif /*SUPERTUX_BUTTON_H*/
+#endif /* SUPERTUX_BUTTON_H */
+
+// EOF
