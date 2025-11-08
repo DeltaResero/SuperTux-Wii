8d8094a649718d46f8410e73367b58686652e97f
DeltaResero <3397617+DeltaResero@users.noreply.github.com>
2025-11-08 00:54:21 -0400

    Banish performance ghosts from alpha blending
    Our rendering engine was seeing ghosts. There were basically temporary copies of
    sprites being created and destroyed in the blink of an eye. Whenever a semi transparent
    sprite was drawn, the old code would create a new temporary surface, blit the sprite onto
    it, apply the transparency, draw it to the screen, and then immediately discard it. This
    happened EVERY single frame, causing a significant and unnecessary performance hit from
    the constant memory allocations. This should help a lot in levels with lots of transparent
    effects. Now our sprites can now be transparent without terrifying the frame rate.

--- src/texture.cpp
+++ src/texture.cpp
@@ -38,8 +38,7 @@ Surface::Surfaces Surface::surfaces;
 SurfaceData::SurfaceData(SDL_Surface* temp, bool use_alpha_)
   : type(SURFACE), surface(nullptr), use_alpha(use_alpha_)
 {
-  // Copy the given surface and make sure that it is not stored in
-  // video memory
+  // Copy the given surface and make sure that it's not stored in video memory
   surface = SDL_CreateRGBSurface(temp->flags & (~SDL_HWSURFACE),
                                  temp->w, temp->h,
                                  temp->format->BitsPerPixel,
@@ -906,34 +905,8 @@ int SurfaceSDL::draw(float x, float y, Uint8 alpha, bool update)
   dest.w = w;
   dest.h = h;
 
-  if (alpha != 255)
-  {
-    /* Create a Surface, make it using colorkey, blit surface into temp, apply alpha
-     * to temp sur, blit the temp into the screen
-     * NOTE: this has to be done, since SDL doesn't allow to set alpha to surfaces that
-     * already have an alpha mask yet... */
-    SDL_Surface* sdl_surface_copy = SDL_CreateRGBSurface(sdl_surface->flags,
-                                    sdl_surface->w, sdl_surface->h, sdl_surface->format->BitsPerPixel,
-                                    sdl_surface->format->Rmask, sdl_surface->format->Gmask,
-                                    sdl_surface->format->Bmask,
-                                    0);
-    int colorkey = SDL_MapRGB(sdl_surface_copy->format, 255, 0, 255);
-    SDL_FillRect(sdl_surface_copy, NULL, colorkey);
-    SDL_SetColorKey(sdl_surface_copy, SDL_SRCCOLORKEY, colorkey);
-
-    SDL_BlitSurface(sdl_surface, NULL, sdl_surface_copy, NULL);
-    SDL_SetAlpha(sdl_surface_copy, SDL_SRCALPHA, alpha);
-
-    int ret = SDL_BlitSurface(sdl_surface_copy, NULL, screen, &dest);
-
-    if (update == UPDATE)
-    {
-      SDL_Flip(screen);
-    }
-
-    SDL_FreeSurface(sdl_surface_copy);
-    return ret;
-  }
+  // Set the alpha on the source surface before blitting.
+  SDL_SetAlpha(sdl_surface, SDL_SRCALPHA, alpha);
 
   int ret = SDL_BlitSurface(sdl_surface, NULL, screen, &dest);
 
@@ -1026,34 +999,8 @@ int SurfaceSDL::draw_part(float sx, float sy, float x, float y, float w, float h
   dest.w = static_cast<int>(w);
   dest.h = static_cast<int>(h);
 
-  if (alpha != 255)
-  {
-    /* Create a Surface, make it using colorkey, blit surface into temp, apply alpha
-     * to temp sur, blit the temp into the screen
-     * Note: this has to be done, since SDL doesn't allow to set alpha to surfaces that
-     * already have an alpha mask yet... */
-    SDL_Surface* sdl_surface_copy = SDL_CreateRGBSurface(sdl_surface->flags,
-                                    sdl_surface->w, sdl_surface->h, sdl_surface->format->BitsPerPixel,
-                                    sdl_surface->format->Rmask, sdl_surface->format->Gmask,
-                                    sdl_surface->format->Bmask,
-                                    0);
-    int colorkey = SDL_MapRGB(sdl_surface_copy->format, 255, 0, 255);
-    SDL_FillRect(sdl_surface_copy, NULL, colorkey);
-    SDL_SetColorKey(sdl_surface_copy, SDL_SRCCOLORKEY, colorkey);
-
-    SDL_BlitSurface(sdl_surface, NULL, sdl_surface_copy, NULL);
-    SDL_SetAlpha(sdl_surface_copy, SDL_SRCALPHA, alpha);
-
-    int ret = SDL_BlitSurface(sdl_surface_copy, NULL, screen, &dest);
-
-    if (update == UPDATE)
-    {
-      SDL_Flip(screen);
-    }
-
-    SDL_FreeSurface(sdl_surface_copy);
-    return ret;
-  }
+  // Set the alpha on the source surface before blitting.
+  SDL_SetAlpha(sdl_surface, SDL_SRCALPHA, alpha);
 
   int ret = SDL_BlitSurface(sdl_surface, &src, screen, &dest);
 
