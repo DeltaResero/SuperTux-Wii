72ad1f96f9a5eb639ac9650ec479734cc194e5a4
DeltaResero <3397617+DeltaResero@users.noreply.github.com>
2025-11-15 20:30:40 -0400

    Revert "Tux learns a little something about head trauma"
    This reverts commit 641e9c64b827e617af288f8dca411388d95be9c0.

--- src/player.cpp
+++ src/player.cpp
@@ -263,11 +263,9 @@ bool Player::on_ground()
  */
 bool Player::under_solid()
 {
-  // A 3-pixel inset is added to prevent corner snags from triggering a head-bonk.
-  const float corner_inset = 3.0f;
   return (issolid(base.x + base.width / 2, base.y) ||
-          issolid(base.x + corner_inset, base.y) ||
-          issolid(base.x + base.width - corner_inset, base.y));
+          issolid(base.x + 1, base.y) ||
+          issolid(base.x + base.width - 1, base.y));
 }
 
 /**
@@ -567,7 +565,7 @@ Sprite* Player::getSpriteFromSet(PlayerSprite* sprite_set)
   {
     return (dir == RIGHT) ? sprite_set->kick_right : sprite_set->kick_left;
   }
-  else if (!on_ground())
+  else if (physic.get_velocity_y() != 0)
   {
     return (dir == RIGHT) ? sprite_set->jump_right : sprite_set->jump_left;
   }
